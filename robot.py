3567761c21120ebcc424362faba11ac562f05598
Daniel Ramos <ramosdaniel1186@gmail.com>
2026-08-17 16:27:20 +0800

    Refactor Robot class to improve battery usage and task performance methods

--- robot.py
+++ robot.py
@@ -26,3 +26,16 @@ class Robot(ABC):
 
     def __repr__(self):
         return f"Robot(name={self.name!r}, battery={self.battery!r})"
+
+    @classmethod
+    def from_config(cls, config_dict):
+        return cls(config_dict["name"], battery=config_dict.get("battery", 100))
+
+    def use_battery(self, amount):
+        if self.battery < amount:
+            raise InsufficientBatteryError(self.name, amount, self.battery)
+        self.battery -= amount
+
+    @abstractmethod
+    def perform_task(self, **kwargs):
+        pass
