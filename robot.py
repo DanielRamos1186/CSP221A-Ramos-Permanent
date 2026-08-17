828afe61d0cf9db151e2eb55c52e4307f79f2c93
Daniel Ramos <ramosdaniel1186@gmail.com>
2026-08-17 16:20:22 +0800

    Refactor Robot class to enhance battery management and string representation

--- robot.py
+++ robot.py
@@ -8,3 +8,21 @@ class Robot(ABC):
         self.name = name
         self.battery = battery
         Robot.population += 1
+
+    @property
+    def battery(self):
+        return self._battery
+
+    @battery.setter
+    def battery(self, value):
+        if value < 0:
+            value = 0
+        if value > 100:
+            value = 100
+        self._battery = value
+
+    def __str__(self):
+        return f"{self.name} ({self.battery}% battery)"
+
+    def __repr__(self):
+        return f"Robot(name={self.name!r}, battery={self.battery!r})"
